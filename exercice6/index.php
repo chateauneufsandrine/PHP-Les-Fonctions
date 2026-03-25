227fe42eef25611bd57792a4e617d8b8c7646d29
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-25 09:25:55 +0100

    add exercice6

--- exercice6/index.php
+++ exercice6/index.php
@@ -2,6 +2,13 @@
 // EXERCICE6///////////////////////////////////////////////////////////////////////////////////////////
 // Faire une fonction qui prend trois paramètres : le nom, le prénom et l'âge d'une personne. Elle doit renvoyer une chaine de la forme :
 // "Bonjour" + nom + prénom + ",tu as" + age + "ans"
+// $nom = "CHATEAUNEUF";
+// $prenom = "Sandrine";
+// $age = 43;
+
+function concatenation($printable1, $nom, $prenom,  $printable2, $age, $printable3){
+    return $printable1 . $nom . $prenom .  $printable2 . $age . $printable3 ;
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,5 +20,7 @@
 </head>
 <body>
     
+<h2><?= concatenation("Bonjour ", "Chateauneuf ", "Sandrine ", ", tu as ", 43, " ans") ?></h2>
+
 </body>
 </html>
